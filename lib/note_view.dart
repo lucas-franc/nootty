7d6c0f4104d34a9b47f02dcc1715a9625fcb6214
Lucas França <lucas.fnev@gmail.com>
2024-06-10 21:16:41 -0300

    removing markdown convertion and add toolbar

--- lib/note_view.dart
+++ lib/note_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:flutter_quill/flutter_quill.dart';
 
 class NoteView extends StatefulWidget {
   const NoteView({super.key});
@@ -10,8 +10,33 @@ class NoteView extends StatefulWidget {
 }
 
 class _NoteViewState extends State<NoteView> {
+  QuillController controller = QuillController.basic();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          QuillToolbar.simple(
+            configurations: QuillSimpleToolbarConfigurations(
+              controller: controller,
+              sharedConfigurations: const QuillSharedConfigurations(
+                locale: Locale('de'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: QuillEditor.basic(
+              configurations: QuillEditorConfigurations(
+                controller: controller,
+                sharedConfigurations: const QuillSharedConfigurations(
+                  locale: Locale('de'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
