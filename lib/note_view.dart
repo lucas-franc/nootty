e7de15edc9bde8649d7a972a2666e037d00791d7
Lucas França <lucas.fnev@gmail.com>
2024-06-10 21:11:25 -0300

    restructuring

--- lib/note_view.dart
+++ lib/note_view.dart
@@ -10,52 +10,8 @@ class NoteView extends StatefulWidget {
 }
 
 class _NoteViewState extends State<NoteView> {
-  final quill.QuillController _controller = quill.QuillController.basic();
-  String _markdownText = '';
-
-  void _onTextChanged() {
-    setState(() {
-      _markdownText = _controller.document.toPlainText();
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(_onTextChanged);
-  }
-
-  @override
-  void dispose() {
-    _controller.removeListener(_onTextChanged);
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Markdown Editor'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: quill.QuillEditor.basic(
-              configurations:
-                  quill.QuillEditorConfigurations(controller: _controller),
-            ),
-          ),
-          Divider(),
-          Expanded(
-            flex: 1,
-            child: Markdown(
-              data: _markdownText,
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
